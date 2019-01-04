b322685734ff9622d69b003326c5a86af0d5cb80
Maxim Shelopugin <maxim.shelopugin@gmail.com>
2019-01-04 14:56:22 -0500

    added templates for gridworld and cliffwalker

---
+++ rl_windy_world/gym_windy_world/wgridworld.py
@@ -0,0 +1,14 @@
+import numpy as np
+import gym
+from gym import spaces
+
+class WindyGridworld(gym.Env):
+    def __init__(self):
+
+    def step(self, action):
+        return 0, reward, True, {}
+
+    def reset(self):
+        return 0
+
+    def render(self, mode='human', close=False):
---
+++ rl_windy_world/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gym_windy_world',
+      version='1.01',
+      install_requires=['gym>=0.2.3'],
+      packages=['gym_windy_world'],
+)
