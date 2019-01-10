4ee071bccb419ae747a54431a9e42a6bda7cf22a
Maxim Shelopugin <maxim.shelopugin@gmail.com>
2019-01-10 10:34:15 -0500

    fixed env bug

--- gym_envs/setup.py
+++ gym_envs/setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_envs',
       version='0.0.1',
-      install_requires=['gym']
+      install_requires=['gym'],
+      packages = ['gym_envs', 'gym_envs.envs']
 )  
